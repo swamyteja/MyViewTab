9825923452a5cffce5e0662c29781daf08528032
sridurgasts <sridurgasts@stgit.com>
2023-10-10 17:46:01 +0530

    MyViewTab Cloned

---
+++ src/main/java/com/atlassian/plugins/jiratraining/web/contextproviders/MyContextProvider.java
@@ -0,0 +1,24 @@
+package com.atlassian.plugins.jiratraining.web.contextproviders;
+
+import java.util.Map;
+
+import com.atlassian.jira.util.collect.MapBuilder;
+import com.atlassian.plugin.PluginParseException;
+import com.atlassian.plugin.web.ContextProvider;
+
+public class MyContextProvider implements ContextProvider {
+    private Long itemCount = 4L;
+
+    @Override
+    public void init(Map<String, String> params) throws PluginParseException {
+        if (params.containsKey("itemCount")) {
+            this.itemCount = Long.parseLong(params.get("itemCount"));
+        }
+    }
+
+    @Override
+    public Map<String, Object> getContextMap(Map context) {
+        return MapBuilder.<String, Object>newBuilder()
+                .add("atl.gh.issue.details.tab.count", itemCount).toMap();
+    }
+}
